8a6fc97d5f2594b7d69c819affdfbc48b2c53780
tmurawsk <tmurawsk@mion.elka.pw.edu.pl>
2018-04-22 22:02:01 +0200

    fixed improper transactions showing

--- backend/src/main/java/pl/edu/pw/elka/transactions/domain/EtherscanTransactionsConnector.java
+++ backend/src/main/java/pl/edu/pw/elka/transactions/domain/EtherscanTransactionsConnector.java
@@ -11,39 +11,31 @@ class EtherscanTransactionsConnector implements TransactionsConnector {
     @Override
     public TransactionLists fetchTransactionLists(String address) {
         RestTemplate restTemplate = new RestTemplate();
-        String incomeTransactionsStringJson = restTemplate.getForObject("http://api.etherscan.io/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&sort=asc&apikey=S5343FW214RWWDHQKR5E9ECC43EVGPEH9R", String.class);
-        String outcomeTransactionsStringJson = restTemplate.getForObject("http://api.etherscan.io/api?module=account&action=txlistinternal&address="+address+"&startblock=0&endblock=2702578&sort=asc&apikey=S5343FW214RWWDHQKR5E9ECC43EVGPEH9R", String.class);
+        String transactionsStringJson = restTemplate.getForObject("http://api.etherscan.io/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&sort=asc&apikey=S5343FW214RWWDHQKR5E9ECC43EVGPEH9R", String.class);
 
-        return new TransactionLists(makeIncomeTransactionArray(incomeTransactionsStringJson), makeOutcomeTransactionArray(outcomeTransactionsStringJson));
+        ArrayList<TransactionDetails> incomeTransactionsArray = new ArrayList<>();
+        ArrayList<TransactionDetails> outcomeTransactionsArray = new ArrayList<>();
+
+        fillTransactionArrays(address, transactionsStringJson, incomeTransactionsArray, outcomeTransactionsArray);
+
+        return new TransactionLists(incomeTransactionsArray, outcomeTransactionsArray);
     }
 
-    private ArrayList<TransactionDetails> makeIncomeTransactionArray(String jsonData) {
+    private void fillTransactionArrays(String address, String jsonData, ArrayList<TransactionDetails> incomeArray, ArrayList<TransactionDetails> outcomeArray) {
         JSONObject givenJsonObject = new JSONObject(jsonData);
         JSONArray givenJsonArray = givenJsonObject.getJSONArray("result");
-        ArrayList<TransactionDetails> resultArray = new ArrayList<>();
 
         for(int i = 0; i < givenJsonArray.length(); i++){
             JSONObject object = givenJsonArray.getJSONObject(i);
-            TransactionDetails transactionDetails = new TransactionDetails(object.getString("from"), object.getString("value"));
 
-            resultArray.add(transactionDetails);
+            if(object.getString("from").equals(address)) {
+                TransactionDetails transactionDetails = new TransactionDetails(object.getString("to"), object.getString("value"));
+                outcomeArray.add(transactionDetails);
+            }
+            else if(object.getString("to").equals(address)) {
+                TransactionDetails transactionDetails = new TransactionDetails(object.getString("from"), object.getString("value"));
+                incomeArray.add(transactionDetails);
+            }
         }
-
-        return resultArray;
-    }
-
-    private ArrayList<TransactionDetails> makeOutcomeTransactionArray(String jsonData) {
-        JSONObject givenJsonObject = new JSONObject(jsonData);
-        JSONArray givenJsonArray = givenJsonObject.getJSONArray("result");
-        ArrayList<TransactionDetails> resultArray = new ArrayList<>();
-
-        for(int i = 0; i < givenJsonArray.length(); i++){
-            JSONObject object = givenJsonArray.getJSONObject(i);
-            TransactionDetails transactionDetails = new TransactionDetails(object.getString("to"), object.getString("value"));
-
-            resultArray.add(transactionDetails);
-        }
-
-        return resultArray;
     }
 }

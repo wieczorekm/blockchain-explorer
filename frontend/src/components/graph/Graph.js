9733cc824f2f4b296cc168730dc51bceca608aa4
Adrianna Małkiewicz <adrianna.malkiewicz@gmail.com>
2018-04-26 09:54:54 +0200

    More nodes and forces

--- frontend/src/components/graph/Graph.js
+++ frontend/src/components/graph/Graph.js
@@ -1,30 +1,72 @@
 import React, { PureComponent } from 'react';
 import { select } from 'd3-selection';
-import { forceSimulation } from 'd3-force';
+import { forceSimulation, forceManyBody, forceLink, forceX, forceY } from 'd3-force';
+import './Graph.css';
 
 class Graph extends PureComponent {
     renderGraph = () => {
+        // To be move to component's method
+        function ticked() {
+            selectedLinks.attr('x1', (d) => d.source.x)
+                .attr('y1', (d) => d.source.y)
+                .attr('x2', (d) => d.target.x)
+                .attr('y2', (d) => d.target.y);
+
+            selectedNodes.attr("transform", (d) => `translate(${d.x},${d.y})`);
+        }
+
         const width = 640;
         const height = 480;
+        const r = 20;
+        const fontSize = 16;
+        const textOffset = fontSize / 4;
 
         const nodes = [
-            { x: width / 2, y: height / 2 },
+            { address: 'sampleNode1' },
+            { address: 'sampleNode2' },
+            { address: 'sampleNode3' },
+            { address: 'sampleNode4' },
         ];
+
+        const links = [
+            { source: 'sampleNode1', target: 'sampleNode2' },
+            { source: 'sampleNode1', target: 'sampleNode3' },
+            { source: 'sampleNode1', target: 'sampleNode4' },
+        ]
 
         const graph = select('.graph')
             .append('svg')
             .attr('width', width)
             .attr('height', height);
 
-        forceSimulation(nodes);
+        const simulation = forceSimulation()
+            .force("charge", forceManyBody().strength(-300))
+            .force('link', forceLink().id((d) => d.address).distance(150))
+            .force("x", forceX(width / 2))
+            .force("y", forceY(height / 2))
+            .on('tick', ticked);
 
-        graph.selectAll('.node')
-            .data(nodes)
-            .enter().append('circle')
-            .attr('class', 'node')
-            .attr('r', width / 25)
-            .attr('cx', (d) => d.x)
-            .attr('cy', (d) => d.y);
+        let selectedLinks = graph.selectAll('.link');
+        let selectedNodes = graph.selectAll('.node');
+
+        simulation.nodes(nodes);
+        simulation.force('link').links(links);
+
+        selectedLinks = selectedLinks.data(links)
+            .enter().append('line')
+            .attr('class', 'link');
+
+        selectedNodes = selectedNodes.data(nodes)
+            .enter().append('g')
+            .attr('class', 'node');
+
+        selectedNodes.append('circle')
+            .attr('r', r);
+
+        selectedNodes.append('text')
+            .attr("dx", (d) => r + textOffset)
+            .attr("dy", (d) => textOffset)
+            .text((d) => d.address);
     }
 
     componentDidMount() {

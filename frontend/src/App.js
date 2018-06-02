1303bf65b0ca8805e0926049c1e8eb7bc3978f8b
Adrianna Małkiewicz <adrianna.malkiewicz@gmail.com>
2018-06-02 16:29:32 +0200

    Prepare API communication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import _ from 'lodash';
+
 import Graph from './components/graph/Graph';
+
 import './App.css';
 
-class App extends Component {
+const API_URL = "https://blockchain-explorer-was-taken.herokuapp.com";
+const BLOCKCHAIN_TYPES = { BITCOIN: 'bitcoin', ETHEREUM: 'ethereum' };
+const BLOCKCHAIN_TYPES_VALUES = Object.values(BLOCKCHAIN_TYPES);
 
+class App extends Component {
     constructor(props) {
         super(props);
-            this.state = {
-            message: ""
-        };
+        this.state = { type: BLOCKCHAIN_TYPES_VALUES[0] };
     }
 
-    componentDidMount() {
-        fetch("https://blockchain-explorer-was-taken.herokuapp.com/hello") // move api url to package.json
-            .then(response => response.text())
-            .then(response => this.setState({message: response}));
+    setType = ({ target: { value } }) => this.setState({ type: value });
+
+    renderOption = (type) => <option value={type} key={type}>{_.capitalize(type)}</option>;
+
+    setAddress = ({ target: { value } }) => this.setState({ address: value });
+
+    fetchData = () => {
+        const { type, address } = this.state;
+
+        fetch(`${API_URL}/${type}/transactions/${address}`)
+            .then(response => console.log(response))
     }
 
     render() {
-        const {message} = this.state;
         return (
             <div>
-                <h1>Blockchain Explorer {message}</h1>
-                <select>
-                    <option>Etherum</option>
-                    <option>Bitcoin</option>
+                <h1>Blockchain Explorer</h1>
+                <select onChange={this.setType} value={this.state.type}>
+                    {BLOCKCHAIN_TYPES_VALUES.map(this.renderOption)}
                 </select>
-                <input />
-                <button>Explore</button>
+                <input onChange={this.setAddress} />
+                <button onClick={this.fetchData}>Explore</button>
                 <Graph />
             </div>
         );

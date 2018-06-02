0665f5d3b7f4339e132e9c85e9cf7c1315799d42
wieczorekm <wmarch112@gmail.com>
2018-06-02 16:25:11 +0200

    Disable cors

--- backend/src/main/java/pl/edu/pw/elka/BlockchainExplorerApplication.java
+++ backend/src/main/java/pl/edu/pw/elka/BlockchainExplorerApplication.java
@@ -2,9 +2,24 @@ package pl.edu.pw.elka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BlockchainExplorerApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlockchainExplorerApplication.class, args);
